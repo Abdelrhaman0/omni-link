60d1ea0de5400c020a2aeb6391663319fe39100e
Your Name <you@example.com>
2026-05-16 12:13:18 +0300

    first commit

---
+++ lib/layout/LayoutScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Layoutscreen extends StatelessWidget {
+  const Layoutscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
